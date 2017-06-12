5d8c7df5d7ff13b5489624f72f34ca0e5f8aaad8
Konstantin Kowalski <kostelkow@gmail.com>
2017-06-12 17:01:11 +0000

    minimax compiles

--- ai.hs
+++ ai.hs
@@ -8,7 +8,7 @@ data MoveTree a = MoveTreeLeaf a | MoveTreeNode a [MoveTree a]
 getMoveTreeBoard (MoveTreeLeaf b) = b
 getMoveTreeBoard (MoveTreeNode b _) = b
 
-instance Foldable MoveTree where
+{-instance Foldable MoveTree where
    --foldMap f (MoveTreeLeaf board) = f board
    --foldMap f (MoveTreeNode trees) = map (foldMap f) trees
    foldr f acc (MoveTreeLeaf board) = f board acc
@@ -16,30 +16,24 @@ instance Foldable MoveTree where
 
    foldr f acc (MoveTreeNode board boards@(x:xs)) =
       foldr f (f (getMoveTreeBoard x) acc) (MoveTreeNode board xs)
-
+-}
 
 --a is Board, b is heuristic return
 minimax :: (Ord b) => Int -> (a -> b) -> MoveTree a -> Bool -> Maybe b
 --minimax check tree depth isMaxi = 0
-minimax depth checkScore (MoveTreeLeaf board) _ = checkScore board
+minimax depth checkScore (MoveTreeLeaf board) _ = Just $ checkScore board
 minimax depth checkScore (MoveTreeNode board rest) isMaxi
-   | depth == 0 = checkScore board
-   | otherwise  =
-      foldl (\ currBoard acc ->
-               let v = extractJust $ minimax (depth - 1) checkScore currBoard newIsMaxi
-               in if not $ isJust acc
-                  then Just v
-                  else let accVal = extractJust acc
-                       in Just $ foldAccFunc accVal v)
-            Nothing
-            rest
-         where foldAccFunc = if isMaxi then max else min
-               newIsMaxi = not isMaxi
+   | depth == 0 = Just $ checkScore board
+   | otherwise = helper depth Nothing rest where
+      helper depth acc [] = acc
+      helper depth acc (x:xs) =
+         let newIsMaxi = not isMaxi
+             foldAccFunc = if isMaxi then max else min
+             v = minimax (depth - 1) checkScore x newIsMaxi
+         in case (v, acc) of
+            (Nothing, _)            -> acc
+            (_, Nothing)            -> v
+            (Just v', Just acc')    -> Just $ foldAccFunc acc' v'
 
 
-
-
-         --bestValue = Nothing
-         --    v = mapTree (minimax (depth - 1) (minimax check rest) False) rest
-         --    bestValue = max bestValue b
 
